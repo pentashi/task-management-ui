cdcc5cfd53fd8e5805aae2a75f3b90cf83fce6fb
achapi <112697206+pentashi@users.noreply.github.com>
2024-09-01 18:33:20 +0200

    Update api.js
    modified backend url to https://task-manager-backend-one-sepia.vercel.app/

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://task-manager-backend-loce.vercel.app/';
+const API_BASE_URL = 'https://task-manager-backend-one-sepia.vercel.app/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
